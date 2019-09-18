0c9c8b3338edf56e79bf786176a81058a0d12860
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-09-18 17:59:25 +0900

    WIP: fix about.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-    <h1>About Netoviz</h1>
-    <ul>
-      <li>Network model visualizer.</li>
-      <li>
-        <a href="https://github.com/corestate55/netoviz">
-          https://github.com/corestate55/netoviz
-        </a>
-      </li>
-    </ul>
-  </div>
+  <v-row>
+    <v-col>
+      <v-list>
+        <v-subheader>About Netoviz</v-subheader>
+        <v-list-item-group>
+          <v-list-item
+            v-for="(item, index) in items"
+            v-bind:key="index"
+            v-bind:href="item.link"
+            target="_blank"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-code-tags',
+          text: 'Github repository',
+          link: 'https://github.com/corestate55/netoviz'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped></style>
